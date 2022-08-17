408324f6aea13d3e7288ac59dea3d2b621cab81f
otterlee9043 <otterlee99@gmail.com>
2022-08-17 22:52:21 +0900

    Log stats info in MessageController

--- src/main/java/com/sign/domain/websocket/MessageController.java
+++ src/main/java/com/sign/domain/websocket/MessageController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 
 @Slf4j
 @RestController
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     private final SimpMessageSendingOperations sendingOperations;
-
+    private final WebSocketMessageBrokerStats stats;
     @MessageMapping("/chat/message")
     public void enter (ChatMessage message){
         if (ChatMessage.MessageType.ENTER.equals(message.getType())){
@@ -20,5 +21,6 @@ public class MessageController {
         }
         log.info("message={}", message);
         sendingOperations.convertAndSend("/topic/chat/room/" + message.getRoomId(), message) ;
+        log.info("stats={}",stats.getWebSocketSessionStatsInfo());
     }
 }
--- src/main/java/com/sign/domain/websocket/SessionHandler.java
+++ src/main/java/com/sign/domain/websocket/SessionHandler.java
@@ -1,15 +1,17 @@
 package com.sign.domain.websocket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
+@Slf4j
 public class SessionHandler extends StompSessionHandlerAdapter {
 
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-        super.afterConnected(session, connectedHeaders);
+        log.info("afterConnected");
     }
 
 }
