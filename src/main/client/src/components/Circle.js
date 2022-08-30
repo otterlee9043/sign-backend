ad2832ca0e9b57cfd07699dc4fa1ab70b92c5794
otterlee9043 <otterlee99@gmail.com>
2022-08-30 20:31:21 +0900

    Feat: 본인 좌석 위에 자기자신임을 표시하는 기능 추가
    유저 자신의 좌석 위에는 'I'를 표시하도록 prop을 추가하고 css를 수정

--- src/main/client/src/components/Circle.js
+++ src/main/client/src/components/Circle.js
@@ -1,26 +1,22 @@
 import styles from "./Circle.module.css";
 import smile from "./emojis/smile.svg";
 
-function Seat({ size, state = "empty", emoji }) {
-  const importAll = (require) =>
-    require.keys().reduce((acc, next) => {
-      acc[next.replace("./", "").split(".")[0]] = require(next);
-      return acc;
-    }, {});
+function Seat({ size, state = "empty", emoji, mySeat = false }) {
+  // const importAll = (require) =>
+  //   require.keys().reduce((acc, next) => {
+  //     acc[next.replace("./", "").split(".")[0]] = require(next);
+  //     return acc;
+  //   }, {});
 
-  const images = importAll(
-    require.context("./emojis", false, /\.(png|jpe?g|svg)$/)
-  );
-
-  //console.log(state);
+  // const images = importAll(
+  //   require.context("./emojis", false, /\.(png|jpe?g|svg)$/)
+  // );
 
   return (
     <div className={`${styles.seat} ${styles[state]} ${styles[size]}`}>
-      <span className={styles.emoji}>{emoji}</span>
+      <span className={`${styles.float} ${styles.emoji}`}>{emoji}</span>
+      {mySeat ? <span className={`${styles.float} ${styles.I}`}>I</span> : null}
     </div>
-    // <div className={styles.circle}>
-    //   <img src={circle} alt="circle" />
-    // </div>
   );
 }
 
