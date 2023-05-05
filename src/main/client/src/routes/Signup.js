f6abeeba47e92b0298e71bb547fa7e63b00ca4ba
otterlee9043 <otterlee99@gmail.com>
2023-05-05 23:44:07 +0900

    refactor: error 메시지 띄우는 방식 변경
    Error 메시지를 더 유동적으로 바꾸기 위해서 <Formik /> 대신 useFormik을 사용하도록 수정

--- src/main/client/src/routes/Signup.js
+++ src/main/client/src/routes/Signup.js
@@ -3,62 +3,63 @@ import styles from "./Login.module.css";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
 import SignupValidationMessage from "../components/SignupValidationMessage.js";
 import axios from "axios";
 import * as Yup from "yup";
 
 function Signup() {
   const { store, actions } = useContext(Context);
-  const [emailBlurred, setEmailBlurred] = useState(false);
-  const [usernameBlurred, setUsernameBlurred] = useState(false);
+
+  const usernameSchema = Yup.string()
+    .required("닉네임을 입력하세요!")
+    .min(2, "닉네임은 최소 2글자 이상입니다!")
+    .max(10, "닉네임은 최대 10글자입니다!")
+    .matches(
+      /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
+      "닉네임에 특수문자가 포함되면 안되고 숫자로 시작하면 안됩니다!"
+  );
+  
+  const emailSchema = Yup.string()
+    .required("이메일을 입력하세요!")
+    .email("올바른 이메일 형식이 아닙니다!");
+  
+  const usernameDuplicateCheck = Yup.string().test(
+    "Unique Username",
+    "Username already in use",
+    async (username) => {
+      if (await usernameSchema.isValid(username)) {
+        // return !(await (await fetch(`/api/member/username/${username}/exists`)).json()[
+        //   "duplicate"
+        // ]);
+        const res = await fetch(`/api/member/username/${username}/exists`);
+        console.log(res);
+        const data = await res.json();
+        console.log(data);
+        const result = data[
+          "duplicate"
+        ];
+        console.log(result);
+        if (result)
+          return false;
+        return true;
+      }
+      return true;
+    }
+  );
+
+  const emailDuplicateCheck = Yup.string().test("Unique Email", "Email already in use", async (email) => {
+    if (await emailSchema.isValid(email)) {
+      return !(await (await fetch(`/api/member/email/${email}/exists`)).json()["duplicate"]);
+    }
+    return true;
+  });
+
+
 
   const validationSchema = Yup.object().shape({
-    email: Yup.string().required("이메일을 입력하세요!").email("올바른 이메일 형식이 아닙니다!"),
-    // .when("email", {
-    //   is: emailBlurred,
-    //   then: Yup.string().test("Unique Email", "Email already in use", function (value) {
-    //     return new Promise((resolve, reject) => {
-    //       fetch(`/api/member/email/${value}/exists`).then(async (res) => {
-    //         const data = await res.json();
-    //         console.log(data);
-    //         if (data.duplicate) {
-    //           console.log("if");
-    //           resolve(false);
-    //         }
-    //         console.log("else");
-    //         resolve(true);
-    //       });
-    //     });
-    //   }),
-    // }),
-    // .test(
-    //   "Unique Email",
-    //   "Email already in use",
-    //   async (email) =>
-    //     await (await fetch(`/api/member/email/${email}/exists`)).json()["duplicate"]
-    // ),
-    // return new Promise((resolve, reject) => {
-    //   fetch(`/api/member/email/${value}/exists`).then(async (res) => {
-    //     const data = await res.json();
-    //     console.log(data);
-    //     if (data.duplicate) {
-    //       console.log("if");
-    //       resolve(false);
-    //     }
-    //     console.log("else");
-    //     resolve(true);
-    //   });
-    // });
-    // }),
-    username: Yup.string()
-      .required("닉네임을 입력하세요!")
-      .min(2, "닉네임은 최소 2글자 이상입니다!")
-      .max(10, "닉네임은 최대 10글자입니다!")
-      .matches(
-        /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
-        "닉네임에 특수문자가 포함되면 안되고 숫자로 시작하면 안됩니다!"
-      ),
+    email: emailSchema,
+    username: usernameSchema,
     password: Yup.string()
       .required("패스워드를 입력하세요!")
       .min(8, "비밀번호는 최소 8자리 이상입니다")
@@ -72,12 +73,14 @@ function Signup() {
       .oneOf([Yup.ref("password"), null], "비밀번호가 일치하지 않습니다!"),
   });
 
-  const validationTest = Yup.object().shape({
-    email: Yup.string().test(
-      "Unique Email",
-      "Email already in use",
-      async (email) => await (await fetch(`/api/member/email/${email}/exists`)).json()["duplicate"]
-    ),
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+      password2: "",
+    },
+    validationSchema: validationSchema,
   });
 
   const navigate = useNavigate();
@@ -120,79 +123,76 @@ function Signup() {
           {store.token && store.token != "" && store.token != undefined ? (
             <div>You are logged in with {store.token}</div>
           ) : (
-            <Formik
-              initialValues={{
-                email: "",
-                username: "",
-                password: "",
-                password2: "",
+            <form
+              onSubmit={(e) => {
+                formik.handleSubmit(e);
+                handleClick(values);
               }}
-              validationSchema={validationSchema}
-              validateOnMount={true}
+              autoComplete="off"
             >
-              {({ values, handleSubmit, handleBlur, handleChange, isValid, isSubmitting }) => (
-                <form
-                  onSubmit={(e) => {
-                    handleSubmit(e);
-                    handleClick(values);
+              {formik.touched.username && formik.errors.username ? (
+                <p className={styles["input-error"]}>{formik.errors.username}</p>
+              ) : null}
+              <input
+                className={styles.input}
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                  onBlur={async (e) => {
+                    formik.handleBlur(e);
+                    await usernameDuplicateCheck.validate(formik.values.username);
                   }}
-                  autoComplete="off"
-                >
-                  <SignupValidationMessage field="username"></SignupValidationMessage>
-                  <Field
-                    className={styles.input}
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={values.username}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <ErrorMessage
-                    className={styles["input-error"]}
-                    component="p"
-                    name="email"
-                    validationSchema={validationTest}
-                  />
-                  <Field
-                    className={styles.input}
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <SignupValidationMessage field="password"></SignupValidationMessage>
-                  <Field
-                    className={styles.input}
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <SignupValidationMessage field="password2"></SignupValidationMessage>
-                  <Field
-                    className={styles.input}
-                    type="password"
-                    name="password2"
-                    placeholder="Password Confirmation"
-                    value={values.password2}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <button
-                    className={styles.loginBtn}
-                    type="submit"
-                    disabled={!isValid || isSubmitting}
-                  >
-                    Sign up
-                  </button>
-                </form>
-              )}
-            </Formik>
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <p className={styles["input-error"]}>{formik.errors.email}</p>
+              ) : null}
+              <input
+                className={styles.input}
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={async (e) => {
+                  formik.handleBlur(e);
+                  await emailDuplicateCheck.validate(formik.values.email);
+                  console.log(formik.errors.email);
+                }}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <p className={styles["input-error"]}>{formik.errors.password}</p>
+              ) : null}
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.password2 && formik.errors.password2 ? (
+                <p className={styles["input-error"]}>{formik.errors.password2}</p>
+              ) : null}
+              <input
+                className={styles.input}
+                type="password"
+                name="password2"
+                placeholder="Password Confirmation"
+                value={formik.values.password2}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              <button
+                className={styles.loginBtn}
+                type="submit"
+                disabled={!formik.isValid || formik.isSubmitting}
+              >
+                Sign up
+              </button>
+            </form>
           )}
         </div>
       </div>
