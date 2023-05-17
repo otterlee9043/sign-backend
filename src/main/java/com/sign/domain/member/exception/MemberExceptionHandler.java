db65c99ccc4a7d242e5b84c754ecf64e0364be15
otterlee9043 <otterlee99@gmail.com>
2023-05-17 23:43:37 +0900

    refactor: DTO 이름 변경에 따른 수정

--- src/main/java/com/sign/domain/member/exception/MemberExceptionHandler.java
+++ src/main/java/com/sign/domain/member/exception/MemberExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.sign.domain.member.exception;
 
-import com.sign.domain.member.controller.dto.MemberSignupErrorResult;
+import com.sign.domain.member.controller.dto.AuthErrorResult;
+import com.sign.domain.member.controller.dto.SignupErrorResult;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,7 +21,7 @@ public class MemberExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public MemberSignupErrorResult exHandler(MethodArgumentNotValidException e){
+    public SignupErrorResult exHandler(MethodArgumentNotValidException e){
         log.error("[MethodArgumentNotValidException] ex", e);
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getAllErrors()
@@ -30,7 +30,7 @@ public class MemberExceptionHandler {
                     String errorMessage = error.getDefaultMessage();
                     errors.put(fieldName, errorMessage);
                 });
-        MemberSignupErrorResult errorResult = MemberSignupErrorResult.builder()
+        SignupErrorResult errorResult = SignupErrorResult.builder()
                 .code("BAD")
                 .message("잘못된 양식")
                 .errors(errors)
@@ -41,13 +41,13 @@ public class MemberExceptionHandler {
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler
-    public MemberSignupErrorResult dataDuplicateExceptionHandler(DataDuplicateException e){
+    public SignupErrorResult dataDuplicateExceptionHandler(DataDuplicateException e){
         log.error("[dataDuplicateExceptionHandler] ex", e);
         Map<String, String> errors = new HashMap<>();
         List<String> fields = e.getFields();
         if (fields.contains("username")) errors.put("username", "사용 중인 이름입니다.");
         if (fields.contains("email")) errors.put("email", "사용 중인 이메일입니다.");
-        MemberSignupErrorResult errorResult = MemberSignupErrorResult.builder()
+        SignupErrorResult errorResult = SignupErrorResult.builder()
                 .code("CONFLICT")
                 .message("중복된 입력값")
                 .errors(errors)
