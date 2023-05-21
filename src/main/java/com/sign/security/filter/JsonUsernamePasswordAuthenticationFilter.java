2de0ce2856a20e6864ec60b74672b7c691afae95
otterlee9043 <otterlee99@gmail.com>
2023-05-21 23:38:53 +0900

    feat: 자체 로그인 기능을 위한 필터 & 핸들러 추가

---
+++ src/main/java/com/sign/security/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package com.sign.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class JsonUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private final ObjectMapper objectMapper;
+    public JsonUsernamePasswordAuthenticationFilter(ObjectMapper objectMapper){
+        super("/api/member/login");
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        if (request.getContentType() == null || !request.getContentType().equals("application/json")) {
+            throw new AuthenticationServiceException("Content type 오류");
+        }
+        String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+        Map<String, String> usernamePasswordMap = objectMapper.readValue(messageBody, Map.class);
+
+        String email = usernamePasswordMap.get("email");
+        String password = usernamePasswordMap.get("password");
+
+        UsernamePasswordAuthenticationToken authRequest
+                = new UsernamePasswordAuthenticationToken(email, password);
+
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+}
---
+++ src/main/java/com/sign/security/handler/LoginSuccessHandler.java
@@ -0,0 +1,33 @@
+package com.sign.security.handler;
+
+import com.sign.domain.member.repository.MemberRepository;
+import com.sign.security.JwtProvider;
+import com.sign.security.LoginMember;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        FilterChain chain, Authentication authentication)
+            throws IOException, ServletException {
+        LoginMember loginMember = (LoginMember) authentication.getPrincipal();
+        String email = loginMember.getUsername();
+        String token = jwtProvider.createToken(email, loginMember.getMember().getRole());
+        jwtProvider.sendToken(response, token);
+        log.info("로그인에 성공!!");
+    }
+}
