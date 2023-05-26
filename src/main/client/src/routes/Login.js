42ba0d3f2cba69d8d5d2a1331c64e498402636ca
otterlee9043 <otterlee99@gmail.com>
2023-05-26 23:36:34 +0900

    feat: 소셜 로그인 버튼 추가

--- src/main/client/src/routes/Login.js
+++ src/main/client/src/routes/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Login.module.css";
 import { useContext, useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
@@ -6,26 +6,20 @@ import { Context } from "../store/appContext";
 
 function Login({ msg }) {
   const { store, actions } = useContext(Context);
-  // const context = useContext(Context);
-  // console.log(context);
-  // const store = context.store;
-  // const actions = context.actions;
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMsg, setMessage] = useState("");
-  //const token = sessionStorage.getItem("token");
-  const location = useLocation();
   const navigate = useNavigate();
 
   const handleClick = async () => {
     const opts = {
       method: "POST",
-      body: new URLSearchParams({
-        username: username,
+      body: JSON.stringify({
+        email: email,
         password: password,
       }),
       headers: new Headers({
-        "content-type": "application/x-www-form-urlencoded",
+        "content-type": "application/json",
       }),
     };
     try {
@@ -34,8 +28,7 @@ function Login({ msg }) {
         alert("There has been some errors.");
         return false;
       }
-       navigate("/");
-      return true;
+      navigate("/home");
     } catch (error) {
       console.error("There has been an error login", error);
     }
@@ -46,17 +39,15 @@ function Login({ msg }) {
       <div className={styles.wrapper}>
         <div className={styles.login}>
           <span className={styles.loginText}>Login</span>
-          {errorMsg == "" ? null : (
-            <div className={styles.errorMsg}>{errorMsg}</div>
-          )}
+          {errorMsg === "" ? null : <div className={styles.errorMsg}>{errorMsg}</div>}
           <div>
             <input
               className={styles.input}
               type="text"
-              name="username"
-              placeholder="username"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
+              name="email"
+              placeholder="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             />
             <input
               className={styles.input}
@@ -66,9 +57,30 @@ function Login({ msg }) {
               value={password}
               onChange={(event) => setPassword(event.target.value)}
             />
-            <button className={styles.loginBtn} onClick={handleClick}>
+            <button
+              className={`${styles["login-button"]} ${styles["login-button-blue"]}`}
+              onClick={handleClick}
+            >
               Sign In
             </button>
+            <br></br>
+            <a href="http://localhost:8080/oauth2/authorization/google">
+              <button
+                className={`${styles["login-button"]} ${styles["login-google"]}`}
+                onClick={loginByGoogle}
+              >
+                Google 로그인
+              </button>
+            </a>
+            <a href="http://localhost:8080/oauth2/authorization/kakao">
+              <button
+                className={`${styles["login-button"]} ${styles["login-kakao"]}`}
+                onClick={loginByKakao}
+              >
+                카카오 로그인
+              </button>
+            </a>
+
             <div>
               <Link to="/signup">Sign up</Link>
               {" / "}
