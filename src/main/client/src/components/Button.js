a0df684a30b464e2f7068d25ff3fdb2155708daa
otterlee9043 <otterlee99@gmail.com>
2023-06-21 23:44:56 +0900

    chore: Button 컴포넌트의 파라미터 수정

--- src/main/client/src/components/Button.js
+++ src/main/client/src/components/Button.js
@@ -2,7 +2,11 @@ import styles from "./Button.module.css";
 
 function Button({ text, type, handleClick }) {
   return (
-    <button className={`${styles.btn} ${styles[type]}`} onClick={handleClick}>
+    <button
+      className={`${styles.btn} ${styles[type]}`}
+      onClick={handleClick}
+      type={type === "room" ? "submit" : "button"}
+    >
       {text}
     </button>
   );
--- src/main/client/src/components/NavLinkButton.js
+++ src/main/client/src/components/NavLinkButton.js
@@ -1,7 +1,7 @@
 import styles from "./NavLinkButton.module.css";
 import { Link } from "react-router-dom";
 
-function NavLinkButton({ path, text, type, roomId = null }) {
+function NavLinkButton({ path, text, type }) {
   return (
     <Link to={`/${path}`} className={`${styles.btn} ${styles[type]}`}>
       {text}
