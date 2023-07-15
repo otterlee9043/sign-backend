7f0b6aa94eeae9b6bdf769ee080c079128872947
otterlee9043 <otterlee99@gmail.com>
2023-07-15 13:22:04 +0900

    refactor: 검색용 RoomCard 컴포넌트 분리

--- src/main/client/src/components/RoomCard.js
+++ src/main/client/src/components/RoomCard.js
@@ -6,86 +6,92 @@ import { useContext, useState } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import { useNavigate } from "react-router";
 
-function RoomCard({ type, id, roomName, hostUsername, hostEmail, handleJoinClick }) {
+export function RoomCard({ roomInfo }) {
   const { currentUser } = useContext(CurrentUserContext);
   const [editing, setEditing] = useState(false);
-  const [savedRoomName, setSavedRoomName] = useState(roomName);
-  const [newRoomName, setNewRoomName] = useState(roomName);
-
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [savedRoomName, setSavedRoomName] = useState(roomInfo.roomName);
+  const [newRoomName, setNewRoomName] = useState(roomInfo.roomName);
+  const isHost = roomInfo.hostEmail === currentUser.email;
   const navigate = useNavigate();
 
-  const handleButtonClick = () => {
-    setEditing(!editing);
-    if (roomName !== newRoomName) updateRoomName();
-  };
-
   const updateRoomName = async () => {
-    try {
-      await axios.put(`/classroom/${id}`, {
-        roomName: newRoomName,
-      });
-      setSavedRoomName(newRoomName);
-    } catch (error) {
-      console.error("There has been an error", error);
+    setEditing((editing) => !editing);
+    if (roomInfo.roomName !== newRoomName) {
+      try {
+        await axios.put(`/classroom/${roomInfo.id}`, {
+          roomName: newRoomName,
+        });
+        setSavedRoomName(newRoomName);
+      } catch (error) {
+        console.error("There has been an error", error);
+      }
     }
   };
 
   const deleteRoomName = async () => {
     try {
-      await axios.delete(`/classroom/${id}`, opts);
-      navigate(0);
+      await axios.delete(`/classroom/${roomInfo.id}`);
+      // navigate(0);
+      setIsDeleted(true);
     } catch (error) {
       console.error("There has been an error", error);
     }
   };
 
-  return type === "입장" ? (
-    editing ? (
-      <div className={styles.card} key={id}>
-        <div className={styles.container}>
-          <div className={styles.roomInfo}>
-            <div className={styles.roomName}>
-              <input
-                size="1"
-                type="text"
-                className={styles["roomName-input"]}
-                value={newRoomName}
-                onChange={(event) => setNewRoomName(event.target.value)}
-              ></input>
+  return (
+    !isDeleted && (
+      <div className={styles.card}>
+        {editing ? (
+          <div className={styles.container}>
+            <div className={styles.roomInfo}>
+              <div className={styles.roomName}>
+                <input
+                  size="1"
+                  type="text"
+                  className={styles["roomName-input"]}
+                  value={newRoomName}
+                  onChange={(event) => setNewRoomName(event.target.value)}
+                ></input>
+              </div>
+              <div className={styles.host}>{roomInfo.hostUsername}</div>
             </div>
-            <div className={styles.host}>{hostUsername}</div>
+            <div className={styles.buttons}>
+              <Button text="설정" type="edit" handleClick={updateRoomName} />
+              <br></br>
+              <Button text="삭제" type="delete" handleClick={deleteRoomName} />
+            </div>
           </div>
-          <div className={styles.buttons}>
-            <Button text="설정" type="edit" handleClick={handleButtonClick} />
-            <br></br>
-            <Button text="삭제" type="delete" handleClick={deleteRoomName} />
+        ) : (
+          <div className={styles.container}>
+            <div className={styles.roomInfo}>
+              <div className={styles.roomName}>{savedRoomName}</div>
+              <div className={styles.host}>{roomInfo.hostUsername}</div>
+            </div>
+            <div className={styles.buttons}>
+              {isHost ? <Button text="설정" type="edit" handleClick={updateRoomName} /> : null}
+              <br></br>
+              <NavLinkButton
+                path={`classroom/${roomInfo.id}`}
+                text="입장"
+                type="enter"
+                roomId={roomInfo.id}
+              />
+            </div>
           </div>
-        </div>
-      </div>
-    ) : (
-      <div className={styles.card} key={id}>
-        <div className={styles.container}>
-          <div className={styles.roomInfo}>
-            <div className={styles.roomName}>{savedRoomName}</div>
-            <div className={styles.host}>{hostUsername}</div>
-          </div>
-          <div className={styles.buttons}>
-            {hostEmail === currentUser.email ? (
-              <Button text="설정" type="edit" handleClick={handleButtonClick} />
-            ) : null}
-            <br></br>
-            {/* <Button text="입장" type="enter" handleClick={handleEnterClick} /> */}
-            <NavLinkButton path={`classroom/${id}`} text="입장" type="enter" roomId={id} />
-          </div>
-        </div>
+        )}
       </div>
     )
-  ) : (
-    <div className={styles.card} key={id}>
+  );
+}
+
+export function EnterRoomCard({ roomInfo, handleJoinClick }) {
+  return (
+    <div className={styles.card}>
       <div className={styles.container}>
         <div className={styles.roomInfo}>
-          <div className={styles.roomName}>{savedRoomName}</div>
-          <div className={styles.host}>{hostUsername}</div>
+          <div className={styles.roomName}>{roomInfo.roomName}</div>
+          <div className={styles.host}>{roomInfo.hostUsername}</div>
         </div>
         <div className={styles.buttons}>
           <br></br>
@@ -95,5 +101,3 @@ function RoomCard({ type, id, roomName, hostUsername, hostEmail, handleJoinClick
     </div>
   );
 }
-
-export default RoomCard;
