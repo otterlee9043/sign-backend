97dc5bb3c21c05c1d19b6ef5c38667466ba3e71b
otterlee9043 <otterlee99@gmail.com>
2023-07-17 23:55:33 +0900

    fix: onBlur에만 validation 작동하도록 수정

--- src/main/client/src/routes/Signup.js
+++ src/main/client/src/routes/Signup.js
@@ -1,10 +1,17 @@
 import styles from "./Login.module.css";
 import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import { validationSchema } from "../schemas/SignupSchema";
+import Button from "@mui/material/Button";
+import theme from "../utils/theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 function Signup() {
+  const [completeEmail, setCompleteEmail] = useState("");
+  const [hasChange, setHasChange] = useState(true);
+  const [validationResult, setValidationResult] = useState(false);
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -12,15 +19,19 @@ function Signup() {
       password: "",
       password2: "",
     },
+    // validationSchema: validationSchema(completeEmail, setCompleteEmail),
+    validationSchema: validationSchema(hasChange, validationResult, setValidationResult),
     validateOnMount: true,
-    validationSchema: validationSchema,
+    validateOnChange: false,
+    validateOnBlur: true,
   });
 
   const navigate = useNavigate();
 
   const handleClick = async (values) => {
+    console.log(values);
     try {
-      const response = await axios.post("/member/join", values);
+      await axios.post("/members", values);
       navigate("/login");
     } catch (error) {
       if (error.response) {
@@ -28,6 +39,13 @@ function Signup() {
         formik.setErrors(data.errors);
       }
     }
+  };
+
+  const onBlur = (e) => {
+    console.log(formik.values.email, completeEmail);
+    setHasChange(formik.values.email !== completeEmail);
+    formik.handleBlur(e);
+    setCompleteEmail(formik.values.email);
   };
 
   return (
@@ -52,7 +70,7 @@ function Signup() {
               placeholder="Username"
               value={formik.values.username}
               onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              onBlur={onBlur}
             />
             {formik.touched.email && formik.errors.email ? (
               <p className={styles["input-error"]}>{formik.errors.email}</p>
@@ -63,8 +81,10 @@ function Signup() {
               name="email"
               placeholder="Email"
               value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              onChange={(e) => {
+                formik.handleChange(e);
+              }}
+              onBlur={onBlur}
             />
             {formik.touched.password && formik.errors.password ? (
               <p className={styles["input-error"]}>{formik.errors.password}</p>
@@ -76,7 +96,7 @@ function Signup() {
               placeholder="Password"
               value={formik.values.password}
               onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              onBlur={onBlur}
             />
             {formik.touched.password2 && formik.errors.password2 ? (
               <p className={styles["input-error"]}>{formik.errors.password2}</p>
@@ -88,14 +108,19 @@ function Signup() {
               placeholder="Password Confirmation"
               value={formik.values.password2}
               onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
+              onBlur={onBlur}
             />
-            <button
-              className={`${styles["login-button"]} ${styles["login-button-blue"]}`}
-              type="submit"
-            >
-              Sign up
-            </button>
+            <ThemeProvider theme={theme}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                type="submit"
+                disabled={!formik.isValid || formik.isSubmitting}
+              >
+                Sign up
+              </Button>
+            </ThemeProvider>
           </form>
         </div>
       </div>
--- src/main/client/src/schemas/SignupSchema.js
+++ src/main/client/src/schemas/SignupSchema.js
@@ -14,25 +14,38 @@ const emailSchema = Yup.string()
   .required("이메일을 입력하세요.")
   .email("올바른 이메일 형식이 아닙니다.");
 
-export const validationSchema = Yup.object().shape({
-  username: usernameSchema,
-  email: emailSchema.test("email", "사용 중인 이메일입니다.", async (email) => {
-    if (await emailSchema.isValid(email)) {
-      const ressponse = await axios.get(`/member/email/${email}/exists`);
-      const result = ressponse.data["duplicate"];
-      return !result;
-    }
-    return true;
-  }),
-  password: Yup.string()
-    .required("비밀번호를 입력하세요.")
-    .min(8, "비밀번호는 최소 8자리 이상입니다")
-    .max(16, "비밀번호는 최대 16자리입니다.")
-    .matches(
-      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[^\s]*$/,
-      "알파벳, 숫자, 공백을 제외한 특수문자를 모두 포함해야 합니다."
-    ),
-  password2: Yup.string()
-    .required("비밀번호를 다시 입력하세요.")
-    .oneOf([Yup.ref("password"), null], "비밀번호가 일치하지 않습니다!"),
-});
+export const validationSchema = (hasChange, previousResult, setValidationResult) =>
+  Yup.object().shape({
+    username: usernameSchema,
+    email: emailSchema.test("email", "사용 중인 이메일입니다.", async (email) => {
+      console.log(hasChange, previousResult);
+      if (hasChange) {
+        if (await emailSchema.isValid(email)) {
+          try {
+            await axios.get(`/members/email/${email}/duplication`);
+            console.log("중복 X, email", email);
+            // setCompleteEmail(email);
+            setValidationResult(true);
+            return true;
+          } catch (error) {
+            console.log("중복 O");
+            setValidationResult(false);
+            return false;
+          }
+        }
+      } else {
+        return previousResult;
+      }
+    }),
+    password: Yup.string()
+      .required("비밀번호를 입력하세요.")
+      .min(8, "비밀번호는 최소 8자리 이상입니다")
+      .max(16, "비밀번호는 최대 16자리입니다.")
+      .matches(
+        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[^\s]*$/,
+        "알파벳, 숫자, 공백을 제외한 특수문자를 모두 포함해야 합니다."
+      ),
+    password2: Yup.string()
+      .required("비밀번호를 다시 입력하세요.")
+      .oneOf([Yup.ref("password"), null], "비밀번호가 일치하지 않습니다!"),
+  });
