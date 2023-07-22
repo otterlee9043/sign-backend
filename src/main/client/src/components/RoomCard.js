6c872be06bf04f5c48fc3f980869f0fbb4abbf08
otterlee9043 <otterlee99@gmail.com>
2023-07-22 23:23:44 +0900

    refactor: 반복되는 코드를 컴포넌트로 분리

--- src/main/client/src/components/RoomCard.js
+++ src/main/client/src/components/RoomCard.js
@@ -1,103 +1,107 @@
 import styles from "./RoomCard.module.css";
-import axios from "axios";
 import NavLinkButton from "./NavLinkButton";
-import Button from "./Button";
-import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Button from "@mui/material/Button";
+import theme from "../utils/theme";
+import { ThemeProvider } from "@mui/material/styles";
+
+import { useContext } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useNavigate } from "react-router";
 
 export function RoomCard({ roomInfo }) {
   const { currentUser } = useContext(CurrentUserContext);
-  const [editing, setEditing] = useState(false);
-  const [isDeleted, setIsDeleted] = useState(false);
-  const [savedRoomName, setSavedRoomName] = useState(roomInfo.roomName);
-  const [newRoomName, setNewRoomName] = useState(roomInfo.roomName);
   const isHost = roomInfo.hostEmail === currentUser.email;
-  const navigate = useNavigate();
 
-  const updateRoomName = async () => {
-    setEditing((editing) => !editing);
-    if (roomInfo.roomName !== newRoomName) {
-      try {
-        await axios.put(`/classroom/${roomInfo.id}`, {
-          roomName: newRoomName,
-        });
-        setSavedRoomName(newRoomName);
-      } catch (error) {
-        console.error("There has been an error", error);
-      }
-    }
-  };
-
-  const deleteRoomName = async () => {
-    try {
-      await axios.delete(`/classroom/${roomInfo.id}`);
-      // navigate(0);
-      setIsDeleted(true);
-    } catch (error) {
-      console.error("There has been an error", error);
-    }
-  };
-
-  return (
-    !isDeleted && (
-      <div className={styles.card}>
-        {editing ? (
-          <div className={styles.container}>
-            <div className={styles.roomInfo}>
-              <div className={styles.roomName}>
-                <input
-                  size="1"
-                  type="text"
-                  className={styles["roomName-input"]}
-                  value={newRoomName}
-                  onChange={(event) => setNewRoomName(event.target.value)}
-                ></input>
-              </div>
-              <div className={styles.host}>{roomInfo.hostUsername}</div>
-            </div>
-            <div className={styles.buttons}>
-              <Button text="설정" type="edit" handleClick={updateRoomName} />
-              <br></br>
-              <Button text="삭제" type="delete" handleClick={deleteRoomName} />
-            </div>
-          </div>
-        ) : (
-          <div className={styles.container}>
-            <div className={styles.roomInfo}>
-              <div className={styles.roomName}>{savedRoomName}</div>
-              <div className={styles.host}>{roomInfo.hostUsername}</div>
-            </div>
-            <div className={styles.buttons}>
-              {isHost ? <Button text="설정" type="edit" handleClick={updateRoomName} /> : null}
-              <br></br>
-              <NavLinkButton
-                path={`classroom/${roomInfo.id}`}
-                text="입장"
-                type="enter"
-                roomId={roomInfo.id}
-              />
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  );
+  return isHost ? <HostRoomCard roomInfo={roomInfo} /> : <EnterRoomCard roomInfo={roomInfo} />;
 }
 
-export function EnterRoomCard({ roomInfo, handleJoinClick }) {
+function RoomCardLayout({ roomInfo, buttons }) {
   return (
     <div className={styles.card}>
       <div className={styles.container}>
         <div className={styles.roomInfo}>
           <div className={styles.roomName}>{roomInfo.roomName}</div>
-          <div className={styles.host}>{roomInfo.hostUsername}</div>
+          <div className={styles.host}>
+            {roomInfo.hostUsername} · {roomInfo.capacity}명
+          </div>
         </div>
-        <div className={styles.buttons}>
-          <br></br>
-          <Button text="참여" type="room" handleClick={handleJoinClick} />
-        </div>
+        {buttons}
       </div>
     </div>
+  );
+}
+
+export function HostRoomCard({ roomInfo }) {
+  return (
+    <RoomCardLayout
+      buttons={<HostRoomCardButtons roomInfo={roomInfo} />}
+      roomInfo={roomInfo}
+    ></RoomCardLayout>
+  );
+}
+
+export function EnterRoomCard({ roomInfo }) {
+  return (
+    <RoomCardLayout
+      buttons={<EnterRoomCardButtons roomInfo={roomInfo} />}
+      roomInfo={roomInfo}
+    ></RoomCardLayout>
+  );
+}
+
+function HostRoomCardButtons({ roomInfo }) {
+  return (
+    <div className={styles.buttons}>
+      <ThemeProvider theme={theme}>
+        <Link to={`/updateroom`} state={{ roomInfo: roomInfo }}>
+          <Button variant="contained" color="setting" size="large">
+            설정
+          </Button>
+        </Link>
+        <br></br>
+        <Link to={`/classroom/${roomInfo.id}`}>
+          <Button variant="contained" color="yellow" size="large">
+            입장
+          </Button>
+        </Link>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+function EnterRoomCardButtons({ roomInfo }) {
+  return (
+    <div className={styles.buttons}>
+      <br></br>
+      <NavLinkButton
+        path={`classroom/${roomInfo.id}`}
+        text="입장"
+        type="enter"
+        roomId={roomInfo.id}
+      />
+    </div>
+  );
+}
+
+function SearchRoomCardButtons({ handleJoinClick }) {
+  return (
+    <div className={styles.buttons}>
+      <br></br>
+      <ThemeProvider theme={theme}>
+        <Button variant="contained" color="primary" size="large" onClick={handleJoinClick}>
+          참여
+        </Button>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export function SearchRoomCard({ roomInfo, handleJoinClick }) {
+  return (
+    <RoomCardLayout
+      buttons={<SearchRoomCardButtons handleJoinClick={handleJoinClick} />}
+      roomInfo={roomInfo}
+    ></RoomCardLayout>
   );
 }
