52146e4b383bf0ad343d9ed45dd122b078ccb6dd
otterlee9043 <otterlee99@gmail.com>
2023-07-24 23:48:27 +0900

    move: 프론트엔드 프로젝트 폴더 외부로 이동

--- src/main/client/src/components/SignupValidationMessage.js
+++
@@ -1,8 +0,0 @@
-import styles from "../routes/Login.module.css";
-import { ErrorMessage } from "formik";
-
-function SignupValidationMessage({ field }) {
-  return <ErrorMessage className={styles["input-error"]} component="p" name={field}></ErrorMessage>;
-}
-
-export default SignupValidationMessage;
