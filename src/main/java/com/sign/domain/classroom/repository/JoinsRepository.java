519c617bfc7b440436180304c8eaeab2a6d30be0
otterlee9043 <otterlee99@gmail.com>
2023-07-28 15:36:16 +0900

    fix: Joins 엔티티 기본키 수정 반영

--- src/main/java/com/sign/domain/classroom/repository/JoinsRepository.java
+++ src/main/java/com/sign/domain/classroom/repository/JoinsRepository.java
@@ -1,8 +1,13 @@
 package com.sign.domain.classroom.repository;
 
 import com.sign.domain.classroom.entity.Joins;
+import com.sign.domain.classroom.entity.JoinsId;
+import com.sign.domain.classroom.entity.Room;
+import com.sign.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JoinsRepository extends JpaRepository<Joins, Long> {
+import java.util.Optional;
 
+public interface JoinsRepository extends JpaRepository<Joins, JoinsId> {
+    Optional<Joins> findByRoomAndMember(Room room, Member member);
 }
