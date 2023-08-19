b65209d283881f03b4d7cf75114fe64ebb59c2fd
otterlee9043 <otterlee99@gmail.com>
2023-08-19 17:43:59 +0900

    fix: 복합키 hashCode(), equals() 구현 클래스 변경

--- src/main/java/com/sign/domain/classroom/entity/JoinsId.java
+++ src/main/java/com/sign/domain/classroom/entity/JoinsId.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor
@@ -12,4 +13,18 @@ import java.io.Serializable;
 public class JoinsId implements Serializable {
     private Long member;
     private Long room;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member, room);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        JoinsId joinsId = (JoinsId) obj;
+        return Objects.equals(member, joinsId.getMember()) &&
+                Objects.equals(room, joinsId.getRoom());
+    }
 }
