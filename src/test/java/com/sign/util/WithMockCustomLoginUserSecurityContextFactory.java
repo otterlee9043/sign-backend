78c196e17406200e1e757f57f5d1067b9124de13
otterlee9043 <otterlee99@gmail.com>
2023-09-01 23:58:32 +0900

    add: WithMockCustomLoginUser 어노테이션 추가

---
+++ src/test/java/com/sign/util/WithMockCustomLoginUserSecurityContextFactory.java
@@ -0,0 +1,33 @@
+package com.sign.util;
+
+import com.sign.domain.member.Role;
+import com.sign.domain.member.entity.Member;
+import com.sign.global.security.authentication.LoginMember;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class WithMockCustomLoginUserSecurityContextFactory
+    implements WithSecurityContextFactory<WithMockCustomLoginUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithMockCustomLoginUser annotation) {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        Member member = Member.builder()
+                .username("username")
+                .email("user@email.com")
+                .role(Role.USER)
+                .build();
+        ReflectionTestUtils.setField(member, "id", 999L);
+
+        UserDetails userDetails = new LoginMember(member);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+        securityContext.setAuthentication(authentication);
+
+        return securityContext;
+    }
+}
