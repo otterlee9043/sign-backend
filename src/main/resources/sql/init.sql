0734bcbff39abd24c59bdec776ece0e6fc8fb15f
otterlee9043 <otterlee99@gmail.com>
2023-10-17 15:00:32 +0900

    fix: email 컬럼에 UNIQUE 속성 추가

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -4,9 +4,9 @@ drop table IF EXISTS member;
 
 create TABLE member (
   MEMBER_ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
   username VARCHAR(30) NOT NULL,
   password VARCHAR(255),
-  email VARCHAR(255) NOT NULL,
   picture VARCHAR(255),
   role VARCHAR(255) NOT NULL,
   refresh_token VARCHAR(255),
