002e0385f21533cc72d4fcbb3541ac9e8605af05
otterlee9043 <otterlee99@gmail.com>
2023-10-18 12:15:03 +0900

    fix: 로그아웃 핸들러 삭제

--- src/main/java/com/sign/global/security/handler/CustomLogoutSuccessHandler.java
+++ src/main/java/com/sign/global/security/handler/CustomLogoutSuccessHandler.java
@@ -4,12 +4,14 @@ import com.sign.domain.member.entity.Member;
 import com.sign.global.security.authentication.JwtProvider;
 import com.sign.global.security.authentication.LoginMember;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @RequiredArgsConstructor
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
@@ -18,7 +20,9 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
                                 Authentication authentication) {
+        log.info("logout | authentication: {}", authentication);
         LoginMember loginMember = (LoginMember) authentication.getPrincipal();
+
         Member member = loginMember.getMember();
         member.updateRefreshToken("");
         jwtProvider.revokeRefreshToken(response);
