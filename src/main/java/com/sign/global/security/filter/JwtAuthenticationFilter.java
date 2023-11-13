9954b69f20a781028980124bf7436546bc21befc
otterlee9043 <otterlee99@gmail.com>
2023-11-13 23:01:34 +0900

    fix: requestMatcher 추가하여 필터 통과할 URL 정의

--- src/main/java/com/sign/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sign/global/security/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,11 @@
 package com.sign.global.security.filter;
 
-import com.sign.domain.member.repository.MemberRepository;
-import com.sign.global.security.authentication.JwtProvider;
+import com.sign.global.security.authentication.jwt.JwtProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,20 +13,44 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
 
+    private final RequestMatcher noCheckRequestMatcher;
+
+
     public JwtAuthenticationFilter(JwtProvider jwtProvider) {
+        String[] excludedUris = {
+                "/api/v1/members",
+                "/api/v1/members/email/*/duplication",
+                "/api/v1/refresh/access-token",
+                "/css/**",
+                "/images/**",
+                "/js/**",
+                "/favicon.ico",
+                "/ws/**",
+        };
+
+        AntPathRequestMatcher[] matchers = Arrays.stream(excludedUris)
+                .map(AntPathRequestMatcher::new)
+                .toArray(AntPathRequestMatcher[]::new);
+
+        this.noCheckRequestMatcher = new OrRequestMatcher(matchers);
         this.jwtProvider = jwtProvider;
     }
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        if (noCheckRequestMatcher.matches(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String accessToken = jwtProvider.extractAccessToken(request);
         Authentication authentication = jwtProvider.getAuthentication(accessToken);
         jwtProvider.saveAuthentication(authentication);
